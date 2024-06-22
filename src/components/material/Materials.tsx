55a63cee5ecdcc654be852a6472b6fe82fe83270
ChoEun-Sang <wh94es@gmail.com>
2024-06-22 23:40:05 +0900

    refactor: 빈 데이터 표시 및 영어버전 변경

--- src/components/material/Materials.tsx
+++ src/components/material/Materials.tsx
@@ -1,6 +1,7 @@
 import useMaterialQuery from "@/hooks/queries/useMaterialQuery";
 import React from "react";
 import Material from "./Material";
+import Image from "next/image";
 
 interface MaterialProps {
   id: number;
@@ -13,16 +14,24 @@ interface MaterialProps {
 function Materials() {
   const { data } = useMaterialQuery();
 
-  if (!data) {
-    return <div>NO data</div>;
-  }
-
   return (
-    <ul>
-      {data.map((value: MaterialProps, idx: number) => (
-        <Material key={value.id} data={value} idx={idx} />
-      ))}
-    </ul>
+    <section>
+      <div className="relative w-full h-10 my-4 ">
+        <input
+          type="text"
+          placeholder="Enter search term"
+          className="relative px-4 w-full h-full border-2 border-gray-2 placeholder-text-disabled rounded-md "
+        />
+        <Image src="/search_check_2.svg" alt="search icon" width={24} height={24} className="absolute right-4 top-2" />
+      </div>
+      {data && (
+        <ul>
+          {data.map((value: MaterialProps, idx: number) => (
+            <Material key={value.id} data={value} idx={idx} />
+          ))}
+        </ul>
+      )}
+    </section>
   );
 }
 
